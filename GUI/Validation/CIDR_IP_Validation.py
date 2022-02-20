ab1fabe2c4bd5e1e9316e2e7d9381c61dbc00ace
Sakthivel Ramasamy <53861448+Sakthivel-Ramasamy@users.noreply.github.com>
2022-02-20 20:38:55 +0530

    Create CIDR_IP_Validation.py

---
+++ GUI/Validation/CIDR_IP_Validation.py
@@ -0,0 +1,17 @@
+import ipaddress
+import json
+import os
+import sys
+
+try:
+    file=open(os.path.dirname(__file__)+"/../input.json", "r")
+    json_data=json.load(file)
+    ipaddr=json_data["IP_Address"]
+    ipaddress.ip_network(ipaddr)
+    print("ok")
+except ValueError:
+    output=open(os.path.dirname(__file__)+"/../error.hop", "a")
+    output.truncate(0)
+    output.write("Error")
+    output.close()
+    sys.exit()
