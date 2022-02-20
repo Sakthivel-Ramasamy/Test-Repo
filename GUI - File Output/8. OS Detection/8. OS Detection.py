34300639fe96d2fcac6c532eadaebfdbbbdf8cd1
SAKTHIVEL RAMASAMY <53861448+Sakthivel-Ramasamy@users.noreply.github.com>
2022-02-20 14:33:33 +0530

    Create 8. OS Detection.py

---
+++ GUI - File Output/8. OS Detection/8. OS Detection.py
@@ -0,0 +1,78 @@
+import datetime
+import ipaddress
+import nmap
+import os
+import prettytable
+from scapy.all import *
+import sys
+
+#Start of OS Detection Scanner
+
+def os_detector(ip):
+    os_detection_scanner_start_time=datetime.now()
+    try:
+        nm=nmap.PortScanner()
+        os_scan_values=nm.scan(ip, arguments='-O')['scan'][ip]['osmatch']
+        counthost=1
+        os_detection_output_table = prettytable.PrettyTable(["Number", "IP Address", "OS Vendor", "OS Family", "OS Generation", "OS Details", "OS Common Platform Enumeration (CPE) Details"])
+        os_vendor=os_scan_values[0]['osclass'][0]['vendor']
+        os_family=os_scan_values[0]['osclass'][0]['osfamily']
+        os_generation=""
+        for i in range(len(os_scan_values[0]['osclass'])-1):
+            os_generation+=str(os_scan_values[0]['osclass'][i]['osgen']) + " | "
+        os_generation+=str(os_scan_values[0]['osclass'][len(os_scan_values[0]['osclass'])-1]['osgen'])
+        os_details=os_scan_values[0]['name']
+        os_cpe=""
+        for i in range(len(os_scan_values[0]['osclass'])-1):
+            os_cpe+=str(os_scan_values[0]['osclass'][i]['cpe']) + " | "
+        os_cpe+=str(os_scan_values[0]['osclass'][len(os_scan_values[0]['osclass'])-1]['cpe'])
+        os_detection_output_table.add_row([counthost, ip, os_vendor, os_family, os_generation, os_details, os_cpe])
+    except IndexError:
+        print("\nSome Error Occurred...\Either the Target IP Address is filtering the connections or Not able to handle the response...\nPlease try again later...")
+    except KeyError:
+        print("\nSome Error Occurred...\nEither the Target IP Address is not active or Not able to reach the Target IP Address.\nPlease try again later...")
+    os_detection_scanner_stop_time=datetime.now()
+    output=open("os_detection_out.hop", "a")
+    output.truncate(0)
+    output.write("OS Detection Scanner started at {}".format(os_detection_scanner_start_time))
+    output.write("\n\nOS Detection Results:\n")
+    output.write(str(os_detection_output_table))
+    output.write("\n\nOS Detection Scanner ended at {}".format(os_detection_scanner_stop_time))
+    output.write("\nTotal Scan Duration in Seconds = {}".format(abs(os_detection_scanner_stop_time-os_detection_scanner_start_time).total_seconds()))
+    output.close()
+    exit_process()
+
+#End of OS Detection Scanner
+
+#Start of Exit Process
+
+def exit_process():
+    sys.exit()
+
+#End of Exit Process
+
+#Start of main Function
+
+if __name__=="__main__":
+
+    #Start of Color Code
+
+    os_type=sys.platform
+    if os_type=='win32':
+        os.system('color')
+    
+    #End of Color Code
+
+    try:
+        ipaddr=input("\nEnter the Target IP Address (Default: 127.0.0.1): ")
+        if len(ipaddr)==0:
+            ip='127.0.0.1'
+        else:
+            ip=ipaddr
+        ipaddress.ip_address(ip)
+        os_detector(ip)
+    except ValueError:
+        print("\nInvalid IP Address Entered...")
+        exit_process()
+
+#End of main Function
