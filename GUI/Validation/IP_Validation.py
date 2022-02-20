c4979b177875b2aec7de72f01f5248a58aa06a6f
Sakthivel Ramasamy <53861448+Sakthivel-Ramasamy@users.noreply.github.com>
2022-02-20 19:44:34 +0530

    Update IP_Validation.py

--- GUI/Validation/IP_Validation.py
+++ GUI/Validation/IP_Validation.py
@@ -3,12 +3,16 @@ import json
 import os
 import sys
 
+from black import out
+
 try:
     file=open(os.path.dirname(__file__)+"/../input.json", "r")
     json_data=json.load(file)
     ipaddr=json_data["IP_Address"]
     ipaddress.ip_address(ipaddr)
 except ValueError:
-    output=open(os.path.dirname(__file__)+"/../error.hop", "w")
+    output=open(os.path.dirname(__file__)+"/../output.hop", "a")
+    output.truncate(0)
+    output.write("Error")
     output.close()
     sys.exit()
