2e3f302b6ffdad11f5877db2f35428488793f4bb
Sakthivel Ramasamy <53861448+Sakthivel-Ramasamy@users.noreply.github.com>
2022-02-20 20:25:28 +0530

    Update Trial 1

---
+++ GUI/Host_Discovery/Host_Discovery.py
@@ -0,0 +1,91 @@
+import datetime
+import ipaddress
+import json
+import nmap
+import os
+import prettytable
+import sys
+
+#Start of Host Discovery Scanner
+
+def host_discovery_scanner_using_nmap(network):
+    counthost=0
+    host_discovery_scanner_using_nmap_start_time=datetime.datetime.now()
+    nm=nmap.PortScanner()
+    nm.scan(hosts=network, arguments='-sn')
+    host_list=list(nm.all_hosts())
+    host_list=sorted(host_list, key=ipaddress.IPv4Address)
+    host_discovery_using_nmap_output_table = prettytable.PrettyTable(["Number", "IP Address", "MAC Address", "Vendor"])
+    for host in host_list:
+        counthost+=1
+        try:
+            macaddress=nm[host]['addresses']['mac']
+        except:
+            macaddress="Might be a local interface /\nNot running as a super user /\nError in getting it..."
+        try:
+            manufacturer=nm[host]['vendor']
+            vendor=manufacturer[macaddress]
+        except:
+            vendor="Might be a local interface /\nNot running as a super user /\nError in getting it..."
+        host_discovery_using_nmap_output_table.add_row([counthost, host, macaddress, vendor])
+    host_discovery_scanner_using_nmap_stop_time=datetime.datetime.now()
+    output=open("output.hop", "a")
+    output.truncate(0)
+    output.write("Host Discovery using Nmap Scan started at {}".format(host_discovery_scanner_using_nmap_start_time))
+    output.write("\n\nScanning Please Wait...")
+    output.write("\n(Note: This may take some time)")
+    output.write("\n\nHost Discovery Using Nmap Result:\n")
+    output.write(str(host_discovery_using_nmap_output_table))
+    output.write("\n\nTotal {} hosts are alive in the given network {}".format(counthost, network))    
+    output.write("\nHost Discovery using Nmap Scan ended at {}".format(host_discovery_scanner_using_nmap_stop_time))
+    output.write("\nTotal Scan Duration in Seconds = {}".format(abs(host_discovery_scanner_using_nmap_stop_time-host_discovery_scanner_using_nmap_start_time).total_seconds()))
+    output.close()
+
+def host_discovery_scanner_using_scapy(network):
+    counthost=0
+    host_discovery_scanner_using_scapy_start_time=datetime.now()
+    a=Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24")
+    result=srp(a,timeout=3,verbose=False)[0]
+    host_discovery_using_scapy_output_table = prettytable.PrettyTable(["Number", "IP Address", "MAC Address", "Vendor"])
+    for element in result:
+        counthost+=1
+        macaddress=element[1].hwsrc
+        macaddress=macaddress.replace(":", "").replace("-", "").replace(".","").upper()
+        macaddress_file_contents=open("Mac-Vendors", "r").read()
+        for macaddr in macaddress_file_contents.split("\n"):
+            if macaddr[0:6] == macaddress[0:6]:
+                vendor=macaddr[7:].strip()
+                break
+        host_discovery_using_scapy_output_table.add_row([counthost, element[1].psrc, element[1].hwsrc, vendor])
+    host_discovery_scanner_using_scapy_stop_time=datetime.now()
+    output=open("output.hop", "a")
+    output.truncate(0)
+    output.write("Host Discovery using Scay Scan started at {}".format(host_discovery_scanner_using_scapy_start_time))
+    output.write("\n\nScanning Please Wait...")
+    output.write("\n(Note: This may take negligible time)")
+    output.write("\n\nHost Discovery Using Scapy Result:\n")
+    output.write(str(host_discovery_using_scapy_output_table))
+    output.write("\n\nTotal {} hosts are alive in the given network {}".format(counthost, network))
+    output.write("\nHost Discovery using Scapy Scan ended at {}".format(host_discovery_scanner_using_scapy_stop_time))
+    output.write("\nTotal Scan Duration in Seconds = {}".format(abs(host_discovery_scanner_using_scapy_stop_time-host_discovery_scanner_using_scapy_start_time).total_seconds()))
+    output.close()
+
+#End of Host Discovery Scanner
+
+#Start of main Function
+
+if __name__=="__main__": 
+    
+    file=open(os.path.dirname(__file__)+"/../input.json", "r")
+    json_data=json.load(file)
+    feature=json_data["Method"]
+    network=json_data["IP_Address"]
+    drop_down=json_data["Drop_Down"]
+    if feature=="Host_Discovery":
+        if drop_down=="Nmap":
+            host_discovery_scanner_using_nmap(network)
+        elif drop_down=="Scapy":
+            host_discovery_scanner_using_scapy(network)
+    sys.exit()
+
+#End of main Function
